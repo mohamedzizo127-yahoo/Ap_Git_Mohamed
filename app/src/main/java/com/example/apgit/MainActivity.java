56359f48e6bb805b12f7952b68be803b9b49fef0
mohamedzizo127-yahoo <70855816+mohamedzizo127-yahoo@users.noreply.github.com>
2020-09-14 12:05:44 +0200

    add method to MainActivity
    add method in main activity

--- app/src/main/java/com/example/apgit/MainActivity.java
+++ app/src/main/java/com/example/apgit/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public String returnName(){
+    return "mohamed zizo";
+    }
 
 }
